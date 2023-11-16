ad6d756096d8b2b8c129abd0b215fc9671685ef1
Leekyoohyun <113281054+qpacman@users.noreply.github.com>
2023-11-16 22:01:12 +0900

    Update traning_code_향상시키기.py

--- traning_code_향상시키기.py
+++ traning_code_향상시키기.py
@@ -2,9 +2,9 @@ import numpy as np
 import math
 
 class neuralNetwork:
-    bih=0
-    bho=0
-    eta=0.01
+    bih=0.35        # ]
+    bho=0.60        # ] bias
+    eta=0.5         # 학습률
 
     def __init__(self,wih,who):
         self.wih=wih
@@ -12,14 +12,19 @@ class neuralNetwork:
         self.sig=lambda x: 1/(1+pow(math.e,-x))
         self.sigp=lambda x: (pow(math.e,-x))/(1+pow(math.e,-x))**2
 
+        #self 함수로 class의 모든 장소에서 함수와 변수를 사용가능, pow승을 의미하는 함수임.
+
         pass
-    def train(self,inputs_list,targers_list):
+    def train(self,inputs_list,targets_list):
         inputs=np.array(inputs_list,ndmin=2).T
         targets=np.array(targets_list,ndmin=2).T
         hidden_inputs=np.dot(self.wih,inputs)+self.bih*np.ones((2,1))
         hidden_outputs=self.sig(hidden_inputs)
         final_inputs=np.dot(self.who,hidden_outputs)+self.bho*np.ones((2,1))
         final_outputs=self.sig(final_inputs)
+
+        #인풋 리스트를 행렬로 만들어준 뒤, 전치하여 계산하는 단계. dot이 행렬계산. ones는 1로 이루어진 행렬 구성.
+        #이로 만들어진 히든 인풋을 self.sig에 넣어 히든 아웃풋 취득. 비슷한 과정을 반복
 
         output_errors=final_outputs-targets
         a=np.dot((output_errors*self.sigp(final_inputs)).T,self.who).T
@@ -30,6 +35,7 @@ class neuralNetwork:
         self.who -= self.eta*dEdw_ho
 
         pass
+        
     def query(self,inputs_list):
         inputs = np.array(inputs_list, ndmin=2).T
         hidden_inputs = np.dot(self.wih, inputs)+self.bih*np.ones((2,1))
@@ -52,6 +58,7 @@ targets_list2 = [0.99,0.05]
 inputs_list3 = [0.99,0.99]
 targets_list3 = [0.01,0.99]
 
+#train
 i=0
 
 while i<10000:
