9172cd52bc6507af6104f43bed0012c1b94d1d01
kyoohyunlee <leekyoohyun@igyuhyeon-ui-MacBookAir.local>
2024-04-07 21:34:37 +0900

    Copying Arrays complete

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,7 +1,24 @@
 public class Arrays {
     public static void main(String[] args){
         int[] foo = new int[5];
-        System.out.println(foo.length);
+        System.out.println("(1):  "+foo.length);     // (1)
+
+        double[] squares;       // Declaration
+        squares = new double[100];      // Creation
+        for (int i=0; i<squares.length; i++){
+            squares[i] = i*i;   // Type promotion
+        }
+       // System.out.println("(2):  "+squa);    // (2)
+
+
+        // System.arraycopy
+
+        int[] srcArray = {1,2,3,4,5};
+        int[] destArray = {101,102,103,104,105,106,107};
+        System.arraycopy(srcArray, 1, destArray, 2, 3);
+        for(int i=0; i<destArray.length; i++){
+            System.out.print(destArray[i]+", ");   // (3)
+        }
     }
 
 
